1f7286f763e8b78912526975d2dcbcb0bb480251
Dendral <niwilkin@ucsc.edu>
2019-03-05 19:38:17 -0800

    main menu to game state switch and npc interactions

---
+++ Assets/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+	private GameObject triggerNpc;
+	private bool triggering;
+	
+	public GameObject npcText;
+
+	void Update()
+	{
+		if(triggering)
+		{
+			//print("Player is talking to " + triggerNpc);
+			npcText.SetActive(true);
+
+			if(Input.GetKeyDown(KeyCode.E))
+			{
+				triggering = false;
+				print("Aw, a little girl! Are you lost?");
+			}
+		}
+		else
+		{
+			npcText.SetActive(false);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "NPC")
+		{
+			triggering = true;
+			triggerNpc = other.gameObject;
+		}
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		if(other.tag == "NPC")
+		{
+			triggering = false;
+			triggerNpc = null;
+		}
+	}
+
+}
